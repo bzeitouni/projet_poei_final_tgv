46b2e07a4a929461e8f1909553e3b76992049b52
root <root@localhost.localdomain>
2020-01-20 14:53:09 +0100

    dump added with tgv.sql

---
+++ dump/tgv.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE SNCF_db
+
+CREATE TABLE Ville
+(
+    codepost INT NOT NULL,
+    nomville VARCHAR(100),
+    PRIMARY KEY (`codepost`)
+)
+CREATE TABLE Gare
+(
+    idgare INT NOT NULL,
+    nomgare VARCHAR(100),
+    codepost INT,
+    PRIMARY KEY (`idgare`),
+    FOREIGN KEY (`codepost`) REFERENCES Ville(`codepost`)
+)
+CREATE TABLE TGV
+(
+    idtrain INT NOT NULL,
+    idgaredepart INT,
+    idgarearrivee INT,
+    heuredepart VARCHAR(100),
+    heurearrivee DATETIME,
+    PRIMARY KEY (`idtrain`),
+    FOREIGN KEY (`idgaredepart`) REFERENCES Gare(`idgare`),
+    FOREIGN KEY (`idgarearrivee`) REFERENCES Gare(`idgare`)
+)
